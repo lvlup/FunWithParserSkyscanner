63c00b56927f599ff4bf6eebe65f0fded1b42cab
lvl_up <russochinskiy@gmail.com>
2016-08-05 02:33:43 +0200

    Правки во вью результатов + добавил атрибуты отображения  имен свойств для RouteInfo и RouteInfoVM

--- ParserFlights/Models/RouteInfo.cs
+++ ParserFlights/Models/RouteInfo.cs
@@ -1,12 +1,19 @@
-﻿namespace ParserFlights.Models
+﻿using System.ComponentModel;
+
+namespace ParserFlights.Models
 {
    public class RouteInfo
     {
+        [DisplayName("Авиакомпания")]
         public string AviaCompanyName { get; set; }
+
+        [DisplayName("Номер рейса")]
         public string RouteNumber { get; set; }
 
+        [DisplayName("Время отбытия")]
         public string DepartureTime { get; set; }
 
+        [DisplayName("Время прибытия")]
         public string ArrivalTime { get; set; }
     }
 }
--- ParserFlights/ViewModels/RouteInfoVM.cs
+++ ParserFlights/ViewModels/RouteInfoVM.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
 using ParserFlights.Models;
 
 namespace ParserFlights.ViewModels
 {
    public class RouteInfoVM
     {
+        [DisplayName("Цена")]
         public string Price { get; set; }
         public List<RouteInfo> Routes { get; set; } 
     }
